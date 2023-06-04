5d6c85dea7c6cffc9ba4aeb6336e5fba62796b79
vanshika2812 <77195888+vanshika2812@users.noreply.github.com>
2023-06-04 13:11:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "links") {
+    x.className += " responsive";
+  } else {
+    x.className = "links";
+  }
+}
